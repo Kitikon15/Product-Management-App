17c18a2d20a939348328b3b2676bf82b849154f5
Kitikon15 <674259003@webmail.npru.ac.th>
2026-09-17 15:55:50 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({path: "../.env"});
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res.status(200).send("<h1>Welcome to Product Management App</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="hover-3d">
+        {/* content */}
+        <figure className="w-60 rounded-2xl">
+          <img
+            src="https://img.daisyui.com/images/stock/card-1.webp?x"
+            alt="Tailwind CSS 3D card"
+          />
+        </figure>
+        {/* 8 empty divs needed for the 3D effect */}
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div className="hover-3d">
+        {/* content */}
+        <figure className="w-60 rounded-2xl">
+          <img
+            src="https://img.daisyui.com/images/stock/card-2.webp?x"
+            alt="Tailwind CSS 3D hover"
+          />
+        </figure>
+        {/* 8 empty divs needed for the 3D effect */}
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div className="hover-3d">
+        {/* content */}
+        <figure className="w-60 rounded-2xl">
+          <img
+            src="https://img.daisyui.com/images/stock/card-3.webp?x"
+            alt="Tailwind CSS 3D hover"
+          />
+        </figure>
+        {/* 8 empty divs needed for the 3D effect */}
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+export default App;
