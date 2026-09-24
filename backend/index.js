033591a6a469c3b3c8c169903ef6bf87a05738bf
Kitikon15 <674259003@webmail.npru.ac.th>
2026-09-24 13:50:21 +0700

    update database.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,25 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./db.js";
+import { connectDB } from "./config/database.js";
+import productRouter from "./router/productRouter.js";
 
-connectDB();
+// .env อยู่ข้างนอก
+dotenv.config();
 
-dotenv.config({path: "../.env"});
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.BACKEND_PORT;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+connectDB();
 app.get("/", (req, res) => {
-    return res.status(200).send("<h1>Welcome to Product Management App</h1>");
+  return res.status(200).send("<h>Hello</h>");
 });
 
+app.use("/api/products", productRouter);
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on: http://localhost:${PORT}`);
 });
